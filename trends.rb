710a4d115399862bad8deaf8f6a46fe154652416
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-07-19 14:03:03 +0100

    Pass timeframe

--- trends.rb
+++ trends.rb
@@ -9,12 +9,12 @@ end
 
 get '/products' do
   content_type :json
-  response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends.json")
+  response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends.json?timeframe=#{params[:timeframe]}")
   response.body.to_json
 end
 
 get '/product/:id' do
   content_type :json
-  response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends/#{params[:id]}.json")
+  response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends/#{params[:id]}.json?timeframe=#{params[:timeframe]}")
   response.body.to_json
 end
