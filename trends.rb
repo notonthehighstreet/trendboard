971e5fb7dc9a2324a3a2e38f986b3c036de90eb5
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-07-19 09:56:28 +0100

    Sinatra app

---
+++ trends.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+require 'httparty'
+require "json"
+
+get '/' do
+  erb :index
+end
+
+get '/trends' do
+  content_type :json
+  response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends.json")
+  response.body.to_json
+end
