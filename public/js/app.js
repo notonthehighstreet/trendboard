4d7e83a3f41add86e200a7f0f21b70dfba6be418
Dan Hart <danhart@notonthehighstreet.com>
2012-07-19 13:16:23 +0100

    products chart show toggle and adjust mustache template

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
-    $.get('/js/templates/product.mustache', function(response) {
-        var product_template = response
 
+    // Loading the product feed
+    var product_template;
+
+    $.get('/js/templates/product.mustache', function(response) {
+        product_template = response;
+        create_products();
+    }, "html");
+
+    function create_products() {
         $.getJSON('/products', function(products) {
             $.each(JSON.parse(products), function(index, product) {
+                product.index = index;
                 var html = Mustache.to_html(product_template, product);
                 $("#product_container").append(html);
             });
         });
-    }, "html");
+    }
+
+    function product_toggles() {
+        $("#page_wrapper").on("click", ".show-chart", function() {
+            var product_module = $(this).closest(".product-module");
+            var trend_chart = product_module.find(".trend-chart");
+            trend_chart.slideToggle();
+            $(this).find("i").toggleClass('icon-arrow-down').toggleClass('icon-arrow-up');
+        });
+    }
+    product_toggles();
 });
