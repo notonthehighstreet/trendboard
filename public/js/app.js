258f44bf8752dcff95c2a7defbb3203a6d9ed7b8
Dan Hart <danhart@notonthehighstreet.com>
2012-07-19 16:15:35 +0100

    Partner stuff

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,76 @@ $(document).ready(function() {
 
     // Loading the product feed
     var product_template;
+    var partner_template;
 
     $.get('/js/templates/product.mustache', function(response) {
         product_template = response;
         create_products();
     }, "html");
 
-    function create_products() {
+    $.get('/js/templates/partner.mustache', function(response) {
+        partner_template = response;
+        create_partners();
+    }, "html");
+
+
+    function create_products(timeframe) {
+        timeframe = typeof timeframe !== 'undefined' ? timeframe : "today";
+
         $("#product_container").empty();
 
-        $.getJSON('/products', function(products) {
+        $.getJSON('/products?timeframe=' + timeframe, function(products) {
             $.each(JSON.parse(products), function(index, product) {
-                product.index = index;
+                product.index = index + 1;
                 var $html = $(Mustache.to_html(product_template, product));
-                $html.data('productId', product.id);
+
+                $html.data('moduleId', product.id);
+                $html.data('moduleType', "product");
+                $html.css("opacity", 0);
+
                 $("#product_container").append($html);
+
+                var delay_offset = 0;
+                $(".module").each(function() {
+                    $(this).delay(delay_offset).animate({
+                        opacity: 1
+                    });
+
+                    delay_offset += 100;
+                });
             });
         });
     }
 
-    function showGraph(d, inserted_product) {
+    function create_partners(timeframe) {
+        timeframe = typeof timeframe !== 'undefined' ? timeframe : "today";
+
+        $("#partner_container").empty();
+
+        $.getJSON('/partner?timeframe=' + timeframe, function(partners) {
+            $.each(JSON.parse(partners), function(index, partner) {
+                partner.index = index + 1;
+                var $html = $(Mustache.to_html(partner_template, partner));
+
+                $html.data('moduleId', partner.id);
+                $html.data('moduleType', "partner");
+                $html.css("opacity", 0);
+
+                $("#partner_container").append($html);
+
+                var delay_offset = 0;
+                $(".module").each(function() {
+                    $(this).delay(delay_offset).animate({
+                        opacity: 1
+                    });
+
+                    delay_offset += 100;
+                });
+            });
+        });
+    }
+
+    function showGraph(d, module) {
         var table = new google.visualization.DataTable();
         table.addColumn('string','Date');
         table.addColumn('number','Sales');
@@ -29,28 +79,37 @@ $(document).ready(function() {
         table.addRows(d);
         var options = {
             hAxis: {title: 'Date',  titleTextStyle: {color: 'red'}},
-            width: inserted_product.width(),
-            height: inserted_product.width() * 0.3
+            width: module.width(),
+            height: module.width() * 0.3,
+            series: [{},{targetAxisIndex:1}]
         };
 
-        var chart = new google.visualization.AreaChart(inserted_product.find('.trend-chart')[0]);
+        var chart = new google.visualization.AreaChart(module.find('.trend-chart')[0]);
         chart.draw(table, options);
     }
 
     // Toggle the container of the chart and do the ajax call
-    function product_toggles() {
+    function module_chart_toggle() {
         $("#page_wrapper").on("click", ".show-chart", function() {
-            var product_module = $(this).closest(".product-module");
-            var trend_chart = product_module.find(".trend-chart");
+            var module = $(this).closest(".module");
+            var trend_chart = module.find(".trend-chart");
             trend_chart.slideToggle();
 
             $(this).find("i").toggleClass('icon-arrow-down').toggleClass('icon-arrow-up');
 
-            $.getJSON('/product/' + product_module.data("productId"), function(product_details) {
-                data = JSON.parse(product_details)[product_module.data("productId")];
-                showGraph(data, product_module);
+            $.getJSON('/' + module.data("moduleType") + '/' + module.data("moduleId"), function(product_details) {
+                data = JSON.parse(product_details)[module.data("moduleId")];
+                showGraph(data, module);
             });
         });
     }
-    product_toggles();
+    module_chart_toggle();
+
+    function time_frame_buttons() {
+        $(".timeframe-button").click(function() {
+            create_products($(this).attr("id"));
+            create_partners($(this).attr("id"));
+        });
+    }
+    time_frame_buttons();
 });
--- trends.rb
+++ trends.rb
@@ -18,3 +18,15 @@ get '/product/:id' do
   response = HTTParty.get("http://www.dev.notonthehighstreet.com/product_trends/#{params[:id]}.json?timeframe=#{params[:timeframe]}")
   response.body.to_json
 end
+
+get '/partner' do
+  content_type :json
+  response = HTTParty.get("http://www.dev.notonthehighstreet.com/partner_trends.json?timeframe=#{params[:timeframe]}")
+  response.body.to_json
+end
+
+get '/partner/:id' do
+  content_type :json
+  response = HTTParty.get("http://www.dev.notonthehighstreet.com/partner_trends/#{params[:id]}.json?timeframe=#{params[:timeframe]}")
+  response.body.to_json
+end
